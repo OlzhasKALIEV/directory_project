eb6128185f3267e10a702efb367787db2fc90c87
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2023-08-19 17:17:33 +0600

    создан файл main

--- directory.py
+++ directory.py
@@ -36,16 +36,4 @@ def create():
     print('Запись успешно создана!!!')
 
 
-if __name__ == '__main__':
-    create_database()
 
-    while True:
-        print("Номера команд: \n1 - добавить запись \n2 - выход")
-        user_input = input('Введите номер команды: ')
-
-        if user_input == '1':
-            create()
-        elif user_input == '2':
-            break
-        else:
-            print('Invalid command')
---
+++ main.py
@@ -0,0 +1,15 @@
+from directory import create_database, create
+
+if __name__ == '__main__':
+    create_database()
+
+    while True:
+        print("Номера команд: \n1 - добавить запись \n2 - выход")
+        user_input = input('Введите номер команды: ')
+
+        if user_input == '1':
+            create()
+        elif user_input == '2':
+            break
+        else:
+            print('Invalid command')
